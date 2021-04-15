11b81cefeaa4a36e6012e8f51a639d74131f16c3
techniccontroller <techniccontroller@gmail.com>
2021-04-15 20:16:12 +0200

    fix emergency stop in rd_strategy

--- src/mobrob_behcon/strategies/rd_strategy.py
+++ src/mobrob_behcon/strategies/rd_strategy.py
@@ -16,6 +16,10 @@ class RDStrategy(Strategy):
         rospy.loginfo("Strategy - dock=%s, turn90=%s", str(self.dock.success), str(self.turn90.success))
         if self.dock.success == 0:
             self.dock.success = -1
-            self.activate_exclusive(turn90)
+            self.activate_exclusive(self.turn90)
         if self.turn90.success == 0:
+            self.finish = True
+        if self.dock.success == 99:
+            # emergency stop
+            rospy.loginfo("success==99 -> emergency stop")
             self.finish = True
