28760d691c160e72db35731c94de43ca03f1d011
techniccontroller <techniccontroller@gmail.com>
2023-01-28 00:30:44 +0100

    code cleanup

--- nodes/main_1.py
+++ nodes/main_1.py
@@ -11,10 +11,9 @@ from mobrob_behcon.behaviours.beh_turn import BehTurn
 from mobrob_behcon.behaviours.beh_reached import BehReached
 from mobrob_behcon.strategies.rd_strategy import RDStrategy
 from mobrob_behcon.core.behcon_node import BehConNode
-
+from myrobot_model.srv import AttinyCommand
 
 if __name__ == '__main__':
-    
     print("start")
     input("Press Enter to continue...")
 
@@ -24,38 +23,34 @@ if __name__ == '__main__':
     dock2 = BehaviourGroup("Dock2")
     turn90 = BehaviourGroup("turn90")
     cv = BehConstTransVel("ConstTransVel", trans_vel=0.2)
-    tu = BehTurn("Turn", -90)
+    tu = BehTurn("Turn", degrees=-90)
     lf = BehLimFor("LimFor", stopdistance=0.2, slowdistance=0.5, slowspeed=0.1)
     al = BehAlign("Align", tolerance=0.020, rot_vel=0.2)
     st = BehStop("Stop", stopdistance=0.3)
     re = BehReached("Reached", stopdistance=0.4)
-    #BehSaveRADock srfull = new BehSaveRADock("SaveDock1", 1000, 1000);
-    #BehSaveRADock srright = new BehSaveRADock("SaveDock2", 300, 1000);
+    # BehSaveRADock srfull = new BehSaveRADock("SaveDock1", 1000, 1000);
+    # BehSaveRADock srright = new BehSaveRADock("SaveDock2", 300, 1000);
 
     dock.add(lf, 80)
     dock.add(cv, 50)
     dock.add(al, 75)
     dock.add(st, 80)
     dock.add(re, 60)
-    #dock.add(srfull, 90)
+    # dock.add(srfull, 90)
     robot.add_beh_group(dock)
-    
-    #dock2.add(lf, 80);
-    #dock2.add(cv, 50);
-    #dock2.add(al, 75)
-    #dock2.add(st, 80);
-    #dock2.add(srright, 90);
-    #robot.add_beh_group(dock2)
-    
+
+    # dock2.add(lf, 80);
+    # dock2.add(cv, 50);
+    # dock2.add(al, 75)
+    # dock2.add(st, 80);
+    # dock2.add(srright, 90);
+    # robot.add_beh_group(dock2)
+
     turn90.add(tu, 80)
-    #turn90.add(al, 80)
-    robot.add_beh_group(turn90)
 
+    robot.add_beh_group(turn90)
     robot.add_beh_group(dock)
 
-    robot.add_beh_group(turn90)
-    
     robot.add_strategy(RDStrategy(dock, turn90))
-    
-    
-    robot.start()		  
+
+    robot.start()
